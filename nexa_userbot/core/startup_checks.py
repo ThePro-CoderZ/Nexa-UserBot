9d19d51db43ea0b129cb2f9d28f950613e24ccb4
Akki ThePro <73880884+Akshat7678@users.noreply.github.com>
2021-12-07 12:09:44 +0530

    Update startup_checks.py

--- nexa_userbot/core/startup_checks.py
+++ nexa_userbot/core/startup_checks.py
@@ -13,14 +13,13 @@ async def check_or_set_log_channel():
         if al_log_channel:
             return [True, al_log_channel]
         else:
-            log_channel = await NEXAUB.create_channel(title="Nexa Userbot Logs", description="Logs of your Nexa Userbot")
+            log_channel = await NEXAUB.create_channel(title="Nexa Userbot Logs!", description="Logs of your Nexa Userbot")
             welcome_to_nexaub = f"""
 **Welcome to Nexa Userbot**
 Thanks for trying Nexa Userbot. If you found any error, bug or even a Feature Request please report it at **@TheArjvps**
 
 **⌲ Quick Start,**
-If you don't know how to use this Userbot please send `{Config.CMD_PREFIX}help` in any chat. It'll show all plugins your userbot has. You can use those plugin names to get info about how to use it. Also check out [Docs](https://nexaub.itz-fork.xyz/)
-
+If you don't know how to use this Userbot please send `{Config.CMD_PREFIX}help` in any chat. It'll show all plugins your userbot has. You can use those plugin names to get info about how to use it. 
 
  **~ Nexa Userbot Authors**"""
             log_channel_id = log_channel.id
@@ -28,7 +27,7 @@ If you don't know how to use this Userbot please send `{Config.CMD_PREFIX}help` 
             await NEXAUB.send_message(chat_id=log_channel_id, text=welcome_to_nexaub, disable_web_page_preview=True)
             return [True, log_channel_id]
     except Exception as e:
-        print(f"Error \n\n{e} \n\nPlease check all variables and try again! \nReport this with logs at @NexaUB_Support if the problem persists!")
+        print(f"Error \n\n{e} \n\nPlease check all variables and try again! \nReport this with logs at @TheArjvpsChat if the problem persists!")
         exit()
 
 
