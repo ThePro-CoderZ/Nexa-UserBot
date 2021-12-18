1ac97e6a264be9fa6593b8a70d04dc0ffd0a85ec
Akki ThePro <73880884+Akshat7678@users.noreply.github.com>
2021-12-18 15:04:07 +0530

    Update cool.py

--- nexa_userbot/modules/cool.py
+++ nexa_userbot/modules/cool.py
@@ -2,16 +2,6 @@ import os
 from nexa_userbot.core.main_cmd import nexaub_on_cmd, e_or_r
 
 
-# Help
-CMD_HELP.update(
-    {
-        "cool": f"""
-**Cool,**
-  ✘ `cool` - To send "I'm Cool"
-"""
-    }
-)
-
 mod_file = os.path.basename(__file__)
 
 @nexaub_on_cmd(command="cool", modlue=mod_file)
