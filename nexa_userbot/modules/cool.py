6e182c664b80b6d3ebadbb95bc27d46db90713df
Akki ThePro <73880884+Akshat7678@users.noreply.github.com>
2021-12-19 08:59:06 +0530

    Delete cool.py

--- nexa_userbot/modules/cool.py
+++
@@ -1,9 +0,0 @@
-import os
-from nexa_userbot.core.main_cmd import nexaub_on_cmd, e_or_r
-
-
-mod_file = os.path.basename(__file__)
-
-@nexaub_on_cmd(command="cool", modlue=mod_file)
-async def my_first_custom_plugin(client, message):
-    await e_or_r(nexaub_message=message, msg_text="`I'm Cool!`")
