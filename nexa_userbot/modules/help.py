36a521177ae08020598edbbd295098fab63784b9
Akki ThePro <73880884+Akshat7678@users.noreply.github.com>
2021-12-19 20:01:22 +0530

    Update help.py

--- nexa_userbot/modules/help.py
+++ nexa_userbot/modules/help.py
@@ -16,7 +16,7 @@ HELP.update(
         "**👨‍💻 Dev**": "`eval`",
         "**🗂 Modules**": "`paste`, `short_url`, `search`, `pictools`, `extractor`, `megatools`, `arq`, `telegraph`, `cloud`",
         "**📂 Utils**": "`stickers`, `owner`, `sudos`, `afk`, `globals`, `groups`",
-        "**💬 Quotes** \nNote:- These are direct Commands! NO NEED TO DO HELP! \n""`ask`, `tell`, `gn`, `gm`, `chat`, `wow`, `cool`",
+        "**💬 Quotes"**: "`ask`, `tell`, `gn`, `gm`, `chat`, `wow`, `cool`",
         "\n**Usage**": "`.help` [module_name]"
     }
 )
