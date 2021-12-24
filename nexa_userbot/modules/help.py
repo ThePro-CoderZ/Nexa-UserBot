92d5903cdb85b210d0987a232117e39d9b6d5b5d
Akki ThePro <73880884+Akshat7678@users.noreply.github.com>
2021-12-24 11:18:13 +0530

    Update help.py

--- nexa_userbot/modules/help.py
+++ nexa_userbot/modules/help.py
@@ -13,8 +13,9 @@ from nexa_userbot.core.main_cmd import nexaub_on_cmd, e_or_r
 HELP.update(
     {
         "**⚡ Userbot**": "`alive`, `installer`, `updater`, `repo`",
+        "**🔥 Animations": "`hello`, `hi`",
         "**👨‍💻 Dev**": "`eval`",
-        "**🗂 Modules**": "`paste`, `short_url`, `search`, `pictools`, `extractor`, `megatools`, `arq`, `telegraph`, `cloud`, `tell`, `ask`, `chat`, `wow`, `gn`, `gm`, `cool`,`hi`",
+        "**🗂 Modules**": "`paste`, `short_url`, `search`, `pictools`, `extractor`, `megatools`, `arq`, `telegraph`, `cloud`, `tell`, `ask`, `chat`, `wow`, `gn`, `gm`, `cool`, `hi`, `hello`",
         "**📂 Database**": "`stickers`, `owner`, `sudos`, `afk`, `globals`, `groups`",
         "\n**Usage**": "`.help` [module_name]"
     }
