22d28c6b52ef6fadeb3f2ac8b78eddda3c9ee77e
Akki ThePro <73880884+Akshat7678@users.noreply.github.com>
2021-12-25 08:38:02 +0530

    Update alive.py

--- nexa_userbot/modules/alive.py
+++ nexa_userbot/modules/alive.py
@@ -109,7 +109,7 @@ async def pingme(_, message: Message):
     start = datetime.now()
     end = datetime.now()
     ping_time = (end - start).microseconds / 1000
-    await ping_msg.edit(f"**Pong:** `{ping_time} ms` \n\n ~ **✨ Nexa-Userbot**", disable_web_page_preview=True)
+    await ping_msg.edit(f"**Pong⚡**\nSpeed 》 `{ping_time} ms` \n\n ~ **Im Fastest my Master!**", disable_web_page_preview=True)
 
 # Set custom alive message
 @nexaub_on_cmd(command="setalive", modlue=mod_file)
